d6fbf240d7ce4f3b06cbfff645fc8c84f9d25acf
Nick Martinez <23nicholasmartinez@gmail.com>
2016-02-26 23:54:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surfspot::Application.config.secret_key_base = '6fb89f64fc5eb28e26bac112556bade29c2d3b0f0b8279c918e0d71027d2d95ab7475a395e73eef3fa310e34f8634d826258e9ef5c1f32c658dda61879ddd90c'
